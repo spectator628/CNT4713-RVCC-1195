95cfca7fb350164ed691684f6d4242ee1440897b
spectator628 <46543210+spectator628@users.noreply.github.com>
2019-05-26 22:25:27 -0400

    Update UDPingServer.py

--- chat app/UDPingServer.py
+++ chat app/UDPingServer.py
@@ -1,3 +1,4 @@
+#This code has segment used from https://www.geeksforgeeks.org/simple-chat-room-using-python/
 # Python program to implement server side of chat room. 
 import socket 
 import select 
